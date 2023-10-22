6cd1f63b6ce012672a15e19bf8ac8e2a5f442c5b
yurii-corssa <yurii.corssa@gmail.com>
2023-10-22 21:51:14 +0200

    Refined contact retrieval with pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,15 @@ const { ctrlWrapper } = require("../../helpers");
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res, next) => {
-  const result = await Contact.find({}, "-createdAt -updatedAt");
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 2 } = req.query;
+  const skip = (page - 1) * limit;
+
+  // const result = await Contact.find({ owner }, "-createdAt -updatedAt").populate("owner", "username email subscription");
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  });
 
   res.json(result);
 };
