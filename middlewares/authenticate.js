8cb530fceb79dd97c057b4f2f77261f70fd755a2
yurii-corssa <yurii.corssa@gmail.com>
2023-10-22 16:49:27 +0200

    Improved token validation

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,12 +8,17 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
 
-  if (bearer !== "Bearer") next(requestError(401));
+  if (bearer !== "Bearer") return next(requestError(401));
 
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById({ _id: id });
-    if (!user) next(requestError(401));
+
+    if (!user || !user.token || user.token !== token) {
+      return next(requestError(401));
+    }
+
+    req.user = user;
 
     next();
   } catch {
