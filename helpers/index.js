ee0ccb96174a9e996ad290bf0b51c5c6d323f750
yurii-corssa <yurii.corssa@gmail.com>
2023-10-26 20:08:33 +0200

    Resize and crop avatars

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,13 @@
 const ctrlWrapper = require("./ctrlWrapper");
+const avatarFilter = require("./avatarFilter");
 const handleMongooseError = require("./handleMongooseError");
 const requestError = require("./requestError");
+const resizeAvatar = require("./resizeAvatar");
 
 module.exports = {
   requestError,
   ctrlWrapper,
   handleMongooseError,
+  resizeAvatar,
+  avatarFilter,
 };
