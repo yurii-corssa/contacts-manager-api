ca85e87cda4a2e41051ef678e1ff0f22801a43a6
yurii-corssa <yurii.corssa@gmail.com>
2023-10-26 20:04:33 +0200

    Added avatar upload

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user.js");
 const controllers = require("../../controllers/users");
 
 const router = express.Router();
 
 router.patch(
-  "/",
+  "/subscription",
   authenticate,
   validateBody(schemas.subscriptionSchema),
-  controllers.handleSubscription
+  controllers.updateSubscription
+);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  controllers.updateAvatar
 );
 
 module.exports = router;
