95c67c744eaaf8f7a6f09bb28c9edeec21908f7d
yurii-corssa <yurii.corssa@gmail.com>
2023-10-26 20:15:43 +0200

    Refactored code

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -9,11 +9,7 @@ const register = async (req, res) => {
   const user = await User.findOne({ email });
   if (user) throw requestError(409, "Email in use");
 
-  const avatarURL = gravatar.url(email, {
-    s: "200",
-    d: "mm",
-  });
-
+  const avatarURL = gravatar.url(email, { s: "200", d: "mm" });
   const hashPassword = await bcrypt.hash(password, 10);
   const newUser = await User.create({
     ...req.body,
@@ -26,7 +22,6 @@ const register = async (req, res) => {
       username: newUser.username,
       email: newUser.email,
       subscription: newUser.subscription,
-      avatarURL,
     },
   });
 };
